139464faebefd1ffa034389ebf2ef501b7f6bef1
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-09-25 01:58:42 +0530

    Update week5.py

--- Week 5/week5.py
+++ Week 5/week5.py
@@ -1,43 +1,52 @@
 import sys
 import os
 import xml.etree.ElementTree as elt
-
 %cd /content/drive/My\ Drive/
-folder = "Train-corups/"
-folder1 = "Test-corpus/"
-
+!pwd
+outerfolder = "Train-corups/"
+outerfolder1 = "Test-corpus/"
 Dict = {}
-for _,_,files in os.walk(folder):
-    for file in files:
-        tree = elt.parse(folder+file)
-        r = tree.getroot()
+    
+shpfiles = []
+for dirpath, subdirs, files in os.walk(outerfolder):
+    for x in files:
+        if x.endswith(".xml"):
+            shpfiles.append(os.path.join(dirpath, x))
+for file in shpfiles:
+  tree = elt.parse(file)
+  r = tree.getroot()
 
-        for w in r.iter('w'):
-          word = w.text.strip().lower()
-          tag = w.attrib['pos']
-          if word in Dict:
-            if tag in Dict[word]:
-              Dict[word][tag]+=1
-            else:
-              Dict[word][tag]=1
-          else:
-            innerDict = {}
-            Dict[word] = innerDict
-            innerDict[tag]=1
-  
+  for w in r.iter('w'):
+    word = w.text.strip().lower()
+    tag = w.attrib['pos']
+    if word in Dict:
+      if tag in Dict[word]:
+        Dict[word][tag]+=1
+      else:
+        Dict[word][tag]=1
+    else:
+      innerDict = {}
+      Dict[word] = innerDict
+      innerDict[tag]=1
+
 out = ""
-for _,_,files in os.walk(folder1):
-    for file in files:
-        tree = elt.parse(folder1+file)
-        r = tree.getroot()
-        
-        for w in r.iter('w'):
-          word = w.text.strip().lower()
-          if word in Dict:
-            tag = max(Dict[word], key=Dict[word].get)
-            out+=(word + " : " + tag + "\n")
-          else:
-            out+=(word + " : Undefined\n")
+shpfiles1 = []
+for dirpath, subdirs, files in os.walk(outerfolder1):
+    for x in files:
+        if x.endswith(".xml"):
+            shpfiles1.append(os.path.join(dirpath, x))
+for file in shpfiles1:
+  tree = elt.parse(file)
+  r = tree.getroot()
+
+  for w in r.iter('w'):
+    word = w.text.strip().lower()
+    if word in Dict:
+      tag = max(Dict[word], key=Dict[word].get)
+      out+=(word + " : " + tag + "\n")
+    else:
+      out+=(word + " : Undefined\n")
+
 %cd /content
 
 out1 = open("Week5_output.txt",'w');
