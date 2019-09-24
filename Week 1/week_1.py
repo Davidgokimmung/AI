82a9e908102bed973c1147cffc754b788597da6a
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-09-25 01:03:56 +0530

    Update week_1.py

--- Week 1/week_1.py
+++ Week 1/week_1.py
@@ -4,19 +4,21 @@ import xml.etree.ElementTree as elt
 
 %cd /content/drive/My\ Drive/
 !pwd
-folder = "Train-corups/"
-file = os.walk(folder)
-
+outerfolder = "Train-corups/"
 out = ""
-for _,_,files in os.walk(folder):
-    print(files)
-    for file in files:
-        print(file)
-        t = elt.parse(folder+file)
-        r = tree.getroot()
+    
+shpfiles = []
+for dirpath, subdirs, files in os.walk(outerfolder):
+    for x in files:
+        if x.endswith(".xml"):
+            shpfiles.append(os.path.join(dirpath, x))
+for file in shpfiles:
+  tree = elt.parse(file)
+  r = tree.getroot()
 
-        for w in r.iter('w'):
-          out+=(w.text.strip().lower() + "_" + w.attrib['pos']+"\n")
+  for w in r.iter('w'):
+    out+=(w.text.strip().lower() + "_" + w.attrib['c5']+"\n")
+  
 %cd /content
 out1 = open("Week1_output.txt", "w")
 out1.write(out)
