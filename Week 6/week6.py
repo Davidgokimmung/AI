1dbb051a13289eb464c4d789ea8468595f055318
David Gokimmung <43725247+Davidgokimmung@users.noreply.github.com>
2019-10-16 03:12:58 +0530

    Update week6.py

--- Week 6/week6.py
+++ Week 6/week6.py
@@ -1,15 +1,19 @@
 import sys
 import os
 import xml.etree.ElementTree as elt
-from sklearn.metrics import confusion_matrix 
-from sklearn.metrics import accuracy_score 
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 %cd /content/drive/My\ Drive/
 !pwd
 outerfolder = "Train-corups/"
 outerfolder1 = "Test-corpus/"
 Dict = {}
-    
+tagset = set()
+taglist = []
 shpfiles = []
 for dirpath, subdirs, files in os.walk(outerfolder):
     for x in files:
@@ -22,6 +26,10 @@ for file in shpfiles:
   for w in r.iter('w'):
     word = w.text.strip().lower()
     tag = w.attrib['c5']
+    if tag not in tagset:
+      tagset.add(tag)
+      taglist.append(tag)
+      
     if word in Dict:
       if tag in Dict[word]:
         Dict[word][tag]+=1
@@ -45,19 +53,31 @@ for file in shpfiles1:
 
   for w in r.iter('w'):
     word = w.text.strip().lower()
+    tag1 = w.attrib['c5']
+    if tag1 not in tagset:
+      tagset.add(tag1)
+      taglist.append(tag1)
     if word in Dict:
       tag = max(Dict[word], key=Dict[word].get)
       predicted.append(tag)
     else:
-      predicted.append('Undefined')
+      predicted.append('UNDEFINED')
     actual.append(w.attrib['c5'])
 
+taglist.append("UNDEFINED")
+taglist.sort()
 out = "Confusion Matrix :" + "\n"
 results = confusion_matrix(actual, predicted)
+print("Confusion matrix:\n%s" % results)
 out+=str(results)+"\n"
-out+="Accuracy Score : " + str(accuracy_score(actual, predicted))
-print(out)
+print("Accuracy Score: ",accuracy_score(actual, predicted))
+
 %cd /content
 
 out1 = open("Week6_output.txt",'w');
 out1.write(out)
+
+df_cm = pd.DataFrame(results, index=taglist, columns=taglist)
+plt.figure(figsize = (20,20))
+sn.heatmap(df_cm, cmap='gray_r')
+plt.show()
